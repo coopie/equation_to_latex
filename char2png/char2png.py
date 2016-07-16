f6e139c08e0184544e87bab837a57ce6fbb7bca1
Ruben Fiszel <ruben.fiszel@epfl.ch>
2016-07-16 15:07:09 -0700

    done gen-char

--- char2png/char2png.py
+++ char2png/char2png.py
@@ -2,11 +2,11 @@ import os
 from pytex2png import pytex2png
 
 def main():
-
-        characters = ["\\alpha", "\\beta"]
+        fil = open("characters", "r+")
+        characters = fil.read().replace(" ", "").split(",")
         for c in characters:
-            stri = c.replace("\\", "")
-	    pytex2png.convert(c,"output/"+stri+".png", 784)
+            stri = c.replace("\\", "__")
+	    pytex2png.convert(c,"output/"+stri+".png", 28)
 
 
 if __name__ == "__main__":
